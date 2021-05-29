effe35c768cce97f9365f8a42418c47700348275
pedromaximilian <pedro.maximilian@gmail.com>
2021-05-29 04:43:40 -0300

    added database and datacontext

---
+++ AlkemyTest/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using AlkemyTest.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlkemyTest.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Character> Characters { get; set; }
+
+
+    }
+}
