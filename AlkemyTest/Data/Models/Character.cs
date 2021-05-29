0377a75b872734212b3e1a9af5e28c428273940b
pedromaximilian <pedro.maximilian@gmail.com>
2021-05-29 04:46:18 -0300

    added character and migration

---
+++ AlkemyTest/Data/Models/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlkemyTest.Data.Models
+{
+    public class Character
+    {
+        //TODO: duplicados con excepcion 63
+        public int Id { get; set; }
+        public string Image { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float Weight { get; set; }
+        public string History { get; set; }
+        //public virtual List<Cast> Casts { get; set; }
+    }
+}
---
+++ AlkemyTest/Migrations/20210529054949_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlkemyTest.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<float>(type: "real", nullable: false),
+                    History = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
