6f92b37f965c87d1361c49a4ae7d6e886474afdc
pedromaximilian <pedro.maximilian@gmail.com>
2021-06-02 22:39:55 -0300

    added model for posting without inner objects

---
+++ AlkemyTest/Data/ViewModels/CharacterPostVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlkemyTest.Data.ViewModels
+{
+    public class CharacterPostVM
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(50)]
+        public string Image { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float? Weight { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        public string History { get; set; }
+    }
+}
---
+++ AlkemyTest/Data/ViewModels/MoviePostVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlkemyTest.Data.ViewModels
+{
+    public class MoviePostVM
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(100)]
+        public string Image { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+        [Range(1, 5)]
+        public byte Qualification { get; set; }
+    }
+}
